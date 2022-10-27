0ba78a03cc04e8dbee1133356afce68353e0f4cf
mac <9588532+t3-mac@users.noreply.github.com>
2022-10-27 16:40:53 +0800

    remove sending temporairly placed discover pocket

--- cmd/main.go
+++ cmd/main.go
@@ -8,9 +8,7 @@ import (
 	"github.com/block-api/block-node-example/auth"
 	"github.com/block-api/block-node-example/user"
 	"github.com/block-api/block-node/block"
-	"github.com/block-api/block-node/common/types"
 	"github.com/block-api/block-node/log"
-	"github.com/block-api/block-node/transporter"
 )
 
 func main() {
@@ -25,24 +23,7 @@ func main() {
 	blockNode := block.NewBlockNode(&options)
 
 	blockNode.AddBlock(&authBlock, &userBlock)
-
 	blockNode.Start()
-
-	/**
-	 * temporairly placed pocket sending test
-	 */
-	target := ""
-	payload := transporter.PayloadDiscovery{
-		NodeID:  blockNode.NodeID().String(),
-		Name:    blockNode.GetName(),
-		Version: blockNode.Version(),
-		Event:   transporter.EventConnected,
-		Blocks:  blockNode.Blocks(),
-	}
-
-	pocket := transporter.NewPocket[transporter.PayloadDiscovery](transporter.ChanDiscovery, blockNode.NodeID(), types.NodeID(target), payload)
-	blockNode.Network().Send(pocket)
-	/** ** ** **/
 
 	var osSignal chan os.Signal = make(chan os.Signal)
 	signal.Notify(osSignal, os.Interrupt, syscall.SIGTERM)
